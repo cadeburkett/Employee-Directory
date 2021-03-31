14daed5c8ff3809ed8fe934601c679ad9f2df5d2
Cade <cadeburkett@Cades-MBP.attlocal.net>
2021-03-31 01:38:58 -0700

    filter function working, need to troubleshoot the sorting age function

--- src/Pages/Main.js
+++ src/Pages/Main.js
@@ -14,9 +14,40 @@ function ShowEmployees() {
 
     }, [])
 
+    function sortAge() {
+        const sortedEmployees = state.users.sort((current, next) => {
+            const currentAge = current.dob.age
+            const nextAge = next.dob.age
+            return nextAge.diff(currentAge)
+        })
+        setState(
+            {
+                ...state,
+                filterUsers: sortedEmployees
+            }
+        )
+    }
+
+    function filterName(event) {
+        const filteredNames = state.users.filter((user) => {
+            const userName = user.name.first + " " + user.name.last
+                return userName.toLowerCase().includes(event.target.value.toLowerCase())
+        })
+        setState(
+            {
+                ...state,
+                filterUsers: filteredNames
+            }
+        )
+    }
+
     return (
         <div>
             <h1>Employee Directory</h1>
+            <div>
+                <input onChange={filterName}></input>
+                <button onClick={sortAge}>Sort by Age</button>
+            </div>
             {state.filterUsers.map(user => {
                 return <EmployeeInfo user={user}></EmployeeInfo>
             })}
